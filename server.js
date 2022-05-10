de295483992c76d604efd62a67d4eef66e432dc6
Chris Porter <cap8095@gmail.com>
2022-05-10 16:11:09 -0400

    adding db query

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
+const mysql = require("mysql2");
 const express = require("express");
 const PORT = process.envPORT || 3001;
 const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+//Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    //Your MYSQL username,
+    user: "root",
+    //Your mysql password
+    password: "5055S@msung3",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
 //Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
+});
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
 });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
